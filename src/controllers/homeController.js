716d11980f460b8a1d4d307087e9b4eedd5debee
David Wanderson <daviddonnna@hotmail.com>
2022-09-25 21:50:48 -0300

    teste

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,11 +1,5 @@
 const HomeModel = require('../models/HomeModel');
 
-HomeModel.create({
-    titulo: 'Programador',
-    descricao: 'JS, PHP'
-}).then(dados => console.log(dados))
-.catch(e=> console.log(e))
-
 HomeModel.find()
 .then(dados => console.log(dados))
 .catch(e=> console.log(e))
