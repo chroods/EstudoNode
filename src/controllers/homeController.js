b2507217fa2a4bbef489a9f0dae5f1e771ba4f47
David wanderson <daviddonnna@hotmail.com>
2022-09-30 22:48:23 -0300

    modified:   package-lock.json 	modified:   package.json 	modified:   routes.js 	deleted:    src/controllers/contatoController.js 	modified:   src/controllers/homeController.js 	new file:   src/controllers/loginController.js 	modified:   src/middleware/middleware.js 	new file:   src/models/LoginModel.js 	new file:   src/views/includes/messages.ejs 	new file:   src/views/login.ejs

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -4,21 +4,6 @@ HomeModel.find()
 .then(dados => console.log(dados))
 .catch(e=> console.log(e))
 */
-exports.paginaInicial = (req, res)=>{
-    //req.session.usuario = {nome: 'David', logado: true}
-    /*req.flash('info', 'ola Mundão!');
-    req.flash('error', 'error');
-    req.flash('success', 'sucesso');*/
-    //console.log(req.flash('info'))
-    
-    res.render(`index`,{
-        titulo: 'Titulo da pagina',
-        numeros: [0,1,2,3,4,5,6,7,8,9]
-    });
-    return;
-}
-
-exports.trataPost = (req, res)=>{
-    res.send(req.body);
-    return;
+exports.index = (req, res)=>{
+    res.render(`index`);
 }
--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -1,17 +1,14 @@
 exports.middlewareGlobal = (req, res,next)=>{
-    res.locals.umaVariavelLocal = 'este é o valor';
-    /*
-    if(req.body.cliente){
-        console.log(`vi que vc postou ${req.body.cliente}`)
-    }
-    */
+    res.locals.errors = req.flash('errors');
+    res.locals.success = req.flash('success');
     next()
 }
 
 exports.checkCsrfError = (err, req, res, next)=>{
-    if(err && err.code === 'EBADCSRFTOKEN'){
+    if(err){
         return res.render('404');
     }
+    next();
 }
 
 exports.csrfMiddleware = (req, res, next)=>{
