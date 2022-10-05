0cecb1731752246a2dd20317451727101dff8488
David wanderson <daviddonnna@hotmail.com>
2022-10-04 23:10:24 -0300

    modified:   routes.js 	modified:   src/controllers/loginController.js 	modified:   src/middleware/middleware.js 	modified:   src/models/LoginModel.js 	modified:   src/views/includes/nav.ejs 	modified:   src/views/index.ejs 	new file:   src/views/login-logado.ejs 	modified:   src/views/login.ejs

--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -16,15 +16,45 @@ class Login{
         this.user = null;
     }
 
+    async login(){
+        this.valida();
+        if(this.errors.length > 0) return;
+
+        this.user = await LoginModel.findOne({email: this.body.email})
+
+        if(!this.user){
+            this.errors.push('Usuario não existe.')
+            return;
+        }
+
+        if(!bcryptjs.compareSync(this.body.password, this.user.password)){
+            this.errors.push('Senha invalida.');
+            this.user = null;
+            return;
+        }
+
+    }
+
     async register(){
         this.valida();
         if(this.errors.length > 0) return;
-        try{
-            this.user = await LoginModel.create(this.body)
-        }catch(e){
-            console.log(e)
-        }
+
+        await this.userExists();
+
+        if(this.errors.length > 0) return;
+
+        const salt = bcryptjs.genSaltSync();
+        this.body.password = bcryptjs.hashSync(this.body.password, salt)
+        
+        this.user = await LoginModel.create(this.body)
+        
     }
+
+    async userExists(){
+        this.user = await LoginModel.findOne({email: this.body.email})
+        if(this.user) this.errors.push('Usuario já existi');
+    }
+
 
     valida(){
         this.cleanUp();
