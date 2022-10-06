a4a80fa7713aef2a715dddff40ed726d8e734818
David wanderson <daviddonnna@hotmail.com>
2022-10-05 22:53:13 -0300

    modified:   routes.js 	new file:   src/controllers/contatoController.js 	modified:   src/middleware/middleware.js 	new file:   src/models/ContatoModel.js 	new file:   src/views/contato.ejs 	modified:   src/views/includes/nav.ejs

--- routes.js
+++ routes.js
@@ -2,15 +2,22 @@ const express = require('express');
 const route = express.Router();
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
+const contatoController = require('./src/controllers/contatoController');
+
+const {loginRequired} = require('./src/middleware/middleware');
 
 //Rotas Home
 route.get('/', homeController.index);
 
 // rotas de login
 route.get('/login/index', loginController.index);
-
 route.post('/login/register', loginController.register);
 route.post('/login/login', loginController.login);
 route.get('/login/logout', loginController.logout);
+
+// Rotas de contatos
+route.get('/contato/index', loginRequired,contatoController.index);
+route.post('/contato/register', loginRequired,contatoController.register);
+route.get('/contato/index/:id', loginRequired,contatoController.editIndex);
 
 module.exports = route;
